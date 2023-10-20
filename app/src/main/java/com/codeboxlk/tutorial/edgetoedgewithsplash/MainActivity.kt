f1cdec6974064838ebc9164e8fb84080ceb74f9b
dilankalaksiri <sendtodilanka@gmail.com>
2023-10-20 19:34:56 +0530

    Initial commit

---
+++ app/src/main/java/com/codeboxlk/tutorial/edgetoedgewithsplash/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.codeboxlk.tutorial.edgetoedgewithsplash
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.codeboxlk.tutorial.edgetoedgewithsplash.ui.theme.EdgeToEdgeWithSplashTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        
+        installSplashScreen()
+
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT, Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT, Color.TRANSPARENT
+            )
+        )
+
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            EdgeToEdgeWithSplashTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting(modifier = Modifier.safeDrawingPadding(), name = "Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EdgeToEdgeWithSplashTheme {
+        Greeting("Android")
+    }
+}
